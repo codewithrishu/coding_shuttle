7021647b88ba4a8f8f0852e254156c3e1e803416
= <=>
2024-09-04 23:11:38 +0530

    2.1

---
+++ introductionToSpringBootweek2/src/main/java/com/coddingshuttle/rishu/week2introduction/introductionToSpringBootweek2/IntroductionToSpringBootweek2Application.java
@@ -0,0 +1,13 @@
+package com.coddingshuttle.rishu.week2introduction.introductionToSpringBootweek2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringBootweek2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToSpringBootweek2Application.class, args);
+	}
+
+}
---
+++ introductionToSpringBootweek2/src/test/java/com/coddingshuttle/rishu/week2introduction/introductionToSpringBootweek2/IntroductionToSpringBootweek2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coddingshuttle.rishu.week2introduction.introductionToSpringBootweek2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBootweek2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
